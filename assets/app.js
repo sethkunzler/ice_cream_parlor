317069b3f15aa650c0f24029d1798e94bdab2ec3
Seth Kunzler <sethkunzler@gmail.com>
2024-01-30 16:24:39 -0700

    Fixed html layout

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,12 @@
 // my java script
+const checkoutWindowElem = document.getElementById('checkoutWindow')
+const totalCostElem = document.getElementById('totalCost')
+
+function buyNow() {
+  console.log('buy now')
+}
+
+function addToCart(name) {
+  console.log(`added ${name} to cart`)
+}
+
