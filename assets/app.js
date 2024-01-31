e2a03188b7933db2e43f31bd4c820b745200c076
Seth Kunzler <sethkunzler@gmail.com>
2024-01-30 17:03:22 -0700

    commit before going home

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,59 @@
+// bcw - base code
+
+const iceCream = [
+  {
+  name: 'Cookie Dough',
+  price: 1.25,
+  quantity: 0
+}, 
+{
+  name: 'Vanilla',
+  price: 1,
+  quantity: 0
+}, 
+{
+  name: 'Strawberry',
+  price: 1.25,
+  quantity: 0
+}
+]
+
+const toppings = [
+  {
+  name: 'Sprinkles',
+  price: .25,
+  quantity: 0
+}, 
+{
+  name: 'Chocolate Chips',
+  price: .25,
+  quantity: 0
+},
+{
+  name: 'Cookie Chunks',
+  price: .5,
+  quantity: 0
+}
+]
+const cones = [
+  {
+  name: 'waffle cone',
+  price: .75,
+  quantity: 0
+}, 
+{
+  name: 'waffle bowl',
+  price: 1.25,
+  quantity: 0
+},
+{
+  name: 'small cone',
+  price: .5,
+  quantity: 0
+}
+]
+
+
 // my java script
 const checkoutWindowElem = document.getElementById('checkoutWindow')
 const totalCostElem = document.getElementById('totalCost')
@@ -8,5 +64,6 @@ function buyNow() {
 
 function addToCart(name) {
   console.log(`added ${name} to cart`)
+  
 }
 
